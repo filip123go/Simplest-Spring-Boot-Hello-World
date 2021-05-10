bebfb53fb5d0d6301e1c1ef807a6c77004a15f92
sfilippidis <stavros.filippidis@sword-group.com>
2021-05-10 16:20:02 +0300

    tests added

---
+++ src/test/java/example/smallest/controllers/WelcomeControllerTest.java
@@ -0,0 +1,19 @@
+package example.smallest.controllers;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WelcomeControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void helloWorld() {
+        {
+            assertEquals("Hello world", "Hello world");
+        }
+    }
+    @org.junit.jupiter.api.Test
+    public void testTrue()
+    {
+        assertTrue( true );
+    }
+}
